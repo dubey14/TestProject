b78303eb2285b5920250676e2e4e57278f1da65a
fulltechstack <fulltechstack@gmail.com>
2023-04-18 02:31:24 +0530

    text added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-project';
+  public isSubTextVisible: boolean = false;
+
+  public toggleSubText() {
+    this.isSubTextVisible = this.isSubTextVisible ? false : true;
+  }
 }
